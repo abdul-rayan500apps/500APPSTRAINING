c59e0638cd2edd8ca9a57453ef8738aa6ee4034a
abdul-raya500apps <abdul.rayan+1@mantragroup.com>
2022-03-24 19:19:20 +0530

    commit

---
+++ demo-24/src/router/index.js
@@ -0,0 +1,23 @@
+import vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '../components/HelloWorld'
+import About from "../components/About"
+
+vue.use(Router)
+
+const routes = [
+    {
+        path:'/',
+        name:"Home",
+        component:HelloWorld
+    },
+    {
+        path:'/about',
+        name:"About",
+        component:About
+    }
+]
+
+let router = new Router({routes})
+
+export default router
