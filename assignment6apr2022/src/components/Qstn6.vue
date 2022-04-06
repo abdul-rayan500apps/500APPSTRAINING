529cdf753f3240d83531b64dd20e5439c7c57757
abdul-raya500apps <abdul.rayan+1@mantragroup.com>
2022-04-06 16:57:50 +0530

    child to parent

---
+++ assignment6apr2022/src/components/Qstn6.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+<h2>Ref Keyword </h2>
+<p>using ref keyword to access data</p>
+<p>{{address}}</p>
+</div>
+
+</template>
+
+<script>
+export default {
+    name : "QuestioN6",
+props:{
+    address :{Default : "Flower",type :String}
+}
+}
+</script>
