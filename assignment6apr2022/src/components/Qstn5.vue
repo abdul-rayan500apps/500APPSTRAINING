4370035b8ab198df35048ffa767930798e3854a6
abdul-raya500apps <abdul.rayan+1@mantragroup.com>
2022-04-06 16:57:18 +0530

    function arg

---
+++ assignment6apr2022/src/components/Qstn5.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <button @click="fullnames()">FullName</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "QuestioN5",
+
+  data() {
+    return {
+      first_name: "Mantra",
+
+      last_name: "Technologies",
+    };
+  },
+
+  methods: {
+    fullnames() {
+      var fullname = this.first_name + this.last_name;
+
+      console.log(fullname);
+    },
+  },
+};
+</script>
