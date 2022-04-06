4c06756dbc03953453b09dc6168aba15a54befd4
abdul-raya500apps <abdul.rayan+1@mantragroup.com>
2022-04-06 16:59:20 +0530

    b table

---
+++ assignment6apr2022/src/components/Qstn8.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <center>
+      <b-table striped hover :items="items" :fields="fields"> </b-table>
+    </center>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "QuestioN8",
+
+  data() {
+    return {
+      fields: ["email", "id", "name"],
+
+      items: [
+        {
+          email: "Eliseo@gardner.biz",
+          id: 1,
+          name: "id labore ex et quamlaborum",
+        },
+
+        {
+          email: "Jayne_Kuhic@sydney.com",
+          id: 2,
+          name: "quo vero reiciendisvelitsimiliqueearum",
+        },
+
+        {
+          email: "Nikita@garfield.biz",
+          id: 3,
+          name: "odio adipisci rerumaut animi",
+        },
+
+        { email: "Lew@alysha.tv", id: 4, name: "alias odio sit" },
+      ],
+    };
+  },
+};
+</script>
